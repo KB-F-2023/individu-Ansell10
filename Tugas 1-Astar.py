92fb6275600f19e6ccc0999faec14d2743baeb08
Ansell10 <syomeronansell@gmail.com>
2023-03-30 21:46:45 +0700

    update Tugas 1

--- Tugas 1-Astar.py
+++ Tugas 1-Astar.py
@@ -1,4 +1,34 @@
-def aStarAlgo(start_node, stop_node):
+#Generate graph node/kota dan edge beserta cost
+Graph_nodes = {
+    'A': [('B', 6), ('F', 3)],
+    'B': [('A', 6), ('C', 3), ('D', 2)],
+    'C': [('B', 3), ('D', 1), ('E', 5)],
+    'D': [('B', 2), ('C', 1), ('E', 8)],
+    'E': [('C', 5), ('D', 8), ('I', 5), ('J', 5)],
+    'F': [('A', 3), ('G', 1), ('H', 7)],
+    'G': [('F', 1), ('I', 3)],
+    'H': [('F', 7), ('I', 2)],
+    'I': [('E', 5), ('G', 3), ('H', 2), ('J', 3)],
+}
+
+#Generate SLD setiap node ke goal
+def Heuristic(n):
+    SLD = {
+        'A': 11,
+        'B': 6,
+        'C': 5,
+        'D': 7,
+        'E': 3,
+        'F': 6,
+        'G': 5,
+        'H': 3,
+        'I': 1,
+        'J': 0
+    }
+    return SLD[n]
+
+#Algoritma A*
+def AStar(start_node, stop_node):
     open_set = set(start_node)
     closed_set = set()
     g = {}            
@@ -8,7 +38,7 @@ def aStarAlgo(start_node, stop_node):
     while len(open_set) > 0:
         n = None
         for v in open_set:
-            if n == None or g[v] + heuristic(v) < g[n] + heuristic(n):
+            if n == None or g[v] + Heuristic(v) < g[n] + Heuristic(n):
                 n = v
         if n == stop_node or Graph_nodes[n] == None:
             pass
@@ -48,32 +78,6 @@ def get_neighbors(v):
         return Graph_nodes[v]
     else:
         return None
-    
-def heuristic(n):
-    H_dist = {
-        'A': 11,
-        'B': 6,
-        'C': 5,
-        'D': 7,
-        'E': 3,
-        'F': 6,
-        'G': 5,
-        'H': 3,
-        'I': 1,
-        'J': 0
-    }
-    return H_dist[n]
 
-Graph_nodes = {
-    'A': [('B', 6), ('F', 3)],
-    'B': [('A', 6), ('C', 3), ('D', 2)],
-    'C': [('B', 3), ('D', 1), ('E', 5)],
-    'D': [('B', 2), ('C', 1), ('E', 8)],
-    'E': [('C', 5), ('D', 8), ('I', 5), ('J', 5)],
-    'F': [('A', 3), ('G', 1), ('H', 7)],
-    'G': [('F', 1), ('I', 3)],
-    'H': [('F', 7), ('I', 2)],
-    'I': [('E', 5), ('G', 3), ('H', 2), ('J', 3)],
-}
-
-aStarAlgo('A', 'J')
+#Tentukan start dan goal
+AStar('A', 'J')
